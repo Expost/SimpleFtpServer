c318bf2654395741c1e5287d3540c6fd5ab1d8a1
Expost <tpxkldh@163.com>
2016-05-05 11:03:26 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@
 
 
 int main(int argc,char **argv) {
-    if(argc != 1){
+    if(argc != 3){
         cout << "Please user SimpleFtpServer [ip] [port] to execute this program." << endl;
         return -1;
     }
-    //CFtpTransfer t(argv[1],atoi(argv[2]));
-    CFtpTransfer t("0.0.0.0",5000);
+    CFtpTransfer t(argv[1],atoi(argv[2]));
+    //CFtpTransfer t("0.0.0.0",5000);
     t.run();
     return 0;
 }
